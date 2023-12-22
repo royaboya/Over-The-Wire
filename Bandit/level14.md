28ea3766fd4cabb00a65d57f27bea485250c8058
royaboya <94084201+royaboya@users.noreply.github.com>
2023-12-22 18:39:54 -0500

    Krypton 2

--- Bandit/level14.md
+++ Bandit/level14.md
@@ -1,5 +1,5 @@
 # Level 14
-Given Prompt:
+Given Prompt: The password for the next level can be retrieved by submitting the password of the current level to port 30000 on localhost
 
 Setup
 `ssh bandit@bandit.labs.overthewire.org -p 2220`
--- Krypton/level1.py
+++ Krypton/level1.py
@@ -4,8 +4,27 @@ Prompt:
 The password for level 2 is in the file ‘krypton2’. It is ‘encrypted’ using a simple rotation. It is also in non-standard ciphertext format. When using alpha characters for cipher text it is normal to group the letters into 5 letter clusters, regardless of word boundaries. This helps obfuscate any patterns.
  This file has kept the plain text word boundaries and carried them to the cipher text. Enjoy!
 
+CyberChef rot13: "YRIRY GJB CNFFJBEQ EBGGRA" -> "LEVEL TWO PASSWORD ROTTEN" 
 
+or python
 """
 
 # File contents: YRIRY GJB CNFFJBEQ EBGGRA
 
+def decoderot13(plain_text:str, offset=13) -> str:
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    letter_to_numbers = {alphabet[i]:i for i in range(len(alphabet))}
+    decoded = ""
+
+
+    for letter in plain_text:
+        if letter.isalpha():
+            decoded += alphabet[(letter_to_numbers[letter] + offset) % len(alphabet)]
+        else:
+            decoded += letter    
+
+    print(decoded)
+
+
+
+decoderot13("YRIRY GJB CNFFJBEQ EBGGRA", 13)
